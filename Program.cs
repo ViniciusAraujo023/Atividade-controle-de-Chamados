647cd240bc93dd5baa66eb9f4635b57e9b10d4e1
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-06-29 18:55:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controle_e_Solicitação_de_chamados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Exibir();
+        }
+    }
+
+
+}
